bb9f00180a0903364662efddc10058e6f9ae82d8
Denis <d.lyskovtsev@mail.ru>
2022-09-17 15:00:03 +0400

    Homework3

---
+++ Example03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число дня недели");
+double DayWeek = int.Parse(Console.ReadLine());
+
+if (DayWeek == 6 || DayWeek == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Будни");
+}
