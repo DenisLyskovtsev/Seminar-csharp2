6e3f40d9e79a34969eef296dfe1029a57283296e
Denis <d.lyskovtsev@mail.ru>
2022-09-17 14:58:26 +0400

    Homework2

---
+++ Example002/Program.cs
@@ -0,0 +1,20 @@
+﻿ Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.Write("третьей цифры нет");
+}
+else
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+
+    if (number < 1000)
+    {
+        int result = (number % 100) % 10;
+        Console.Write($"Третья цифра числа = {result}");
+    }
+}
