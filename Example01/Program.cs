e8245b034a1821f79ee080a44393b7b5df62f644
Denis <d.lyskovtsev@mail.ru>
2022-09-17 14:56:34 +0400

    Homework1

---
+++ Example01/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int result1 = number / 10;
+int result2 = result1 % 10;
+Console.Write($"Вторая цифра числа = {result2}");
